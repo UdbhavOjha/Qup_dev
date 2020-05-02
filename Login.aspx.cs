7ed5a935a14923111e4509e001e83d65b9dd96ae
UdbhavOjha <ojha.udbhav@gmail.com>
2020-05-02 14:23:17 +1200

    Add Login Page

---
+++ Login.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Qup
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void login_Click(object sender, EventArgs e)
+        {
+            //var userCredentials = new AuthenticateUserSession
+            //{
+            //    UserName = Request["username"],
+            //    Password = Request["password"],
+            //    IpAddress = Request.UserHostAddress
+            //};
+
+            //var authentication = new UserAuthentication();
+            //var sessionStatus = authentication.AuthenticateUserCredentials(userCredentials);
+
+            //if (!sessionStatus.Equals("failed"))
+            //{
+            //    HttpCookie sessionInfo = new HttpCookie("SessionInfo");
+            //    sessionInfo["Session"] = sessionStatus;
+
+            //    Response.Cookies.Add(sessionInfo);
+            //    Response.Redirect("/Admin/Users/Dashboard.aspx");
+            //}
+        }
+    }
+}
---
+++ PatronSignUp.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Qup
+{
+    public partial class PatronSignUp : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void signUpSubmit_Click(object sender, EventArgs e)
+        {
+            //var formData = GetFormData();
+            //var validator = new NewAccountSignUpFormValidator();
+            //var results = validator.Validate(formData);
+            //bool validationSuccess = results.IsValid;
+            //Failures = results.Errors;
+
+            //if (!validationSuccess)
+            //{
+            //    return;
+            //}
+
+            //// Recaptcha check
+            //if (!IsRecaptchaValid())
+            //{
+            //    RecaptchaError = "Please tick the recaptcha checkbox.";
+            //    return;
+            //}
+        }
+    }
+}
